09fb54ab1ac51d4cce8358ce99c4cceda3549e89
PatroChandan <chandanpatro789@gmail.com>
2023-12-29 12:10:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const h3 = document.getElementById("h3");
+const h2 = document.getElementById("h2");
+const btn = document.querySelector(".btn");
+const navh2 = document.getElementById("navh2");
+const img2 = document.getElementById("2");
+const img3 = document.getElementById("3");
+btn.addEventListener("click", function () {
+  navh2.innerHTML = "Complete";
+  h3.innerHTML = "Thank you! 🎉";
+  h2.innerHTML = "Your order has been received";
+  btn.innerHTML = "Purchase history";
+});
